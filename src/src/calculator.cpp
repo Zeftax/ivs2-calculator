88c9e6b9b45b249146f40a8a0840a07324994911
Tomáš Krejčí <xkrejc84@vutbr.cz>
2023-04-10 16:53:23 +0200

    Created project structure

---
+++ src/src/calculator.cpp
@@ -0,0 +1,25 @@
+/*************************************************
+ * Project name: ivs2-calculator
+ * Date:         2023-04-10
+ *************************************************/
+/**
+ * @file calculator.cpp
+ *
+ * @author Zdeněk Borovec <xborov08@vutbr.cz>
+ * @author Tomáš Krejčí <xkrejc84@vutbr.cz>
+ * @author Jan Lozrt <xlozrt00@vutbr.cz>
+ * @author Jakub Mitrenga <xmitre07@vutbr.cz>
+ *
+ * @brief Simple calculator app
+ */
+
+#include <iostream>
+#include "ivs2-mathlib/ivs-math.hpp"
+
+using namespace std;
+
+int main ()
+{
+    cout << "Hello calculator!" << endl;
+    return 0;
+}
