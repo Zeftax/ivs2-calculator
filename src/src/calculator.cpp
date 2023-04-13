1675b5c31a46087323a668890d0a40659737ad83
Tomáš Krejčí <xkrejc84@vutbr.cz>
2023-04-13 21:36:26 +0200

    Initial GTK4 setup and demo application

--- src/src/calculator.cpp
+++ src/src/calculator.cpp
@@ -16,13 +16,44 @@
  */
 
 #include <iostream>
+#include <gtk/gtk.h>
 #include "ivs-math.hpp"
 
 using namespace std;
 
-int main ()
+static void demo(GtkWidget *widget, gpointer data)
 {
-    double res = ivsm::add(4, 5);
-    cout << "4 + 5 = " << res << endl;
+    g_print("4 + 5 = %lf\n", ivsm::add(4, 5));
+}
+
+static void activate(GtkApplication *app, gpointer user_data)
+{
+    GtkWidget *window;
+    GtkWidget *button;
+
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Ratulator");
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
+
+    button = gtk_button_new_with_label("4 + 5");
+
+    g_signal_connect(button, "clicked", G_CALLBACK(demo), NULL);
+    gtk_window_set_child(GTK_WINDOW(window), button);
+
+    gtk_window_present(GTK_WINDOW(window));
+}
+
+int main(int argc, char **argv)
+{
+    GtkApplication *app;
+    int status;
+
+    // IMPORTANT: first arg is window class!
+    //                          ↓
+    app = gtk_application_new("org.ratulator", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+
     return 0;
 }
