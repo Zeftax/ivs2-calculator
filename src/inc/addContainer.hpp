4cccb0ae8fc98bad6a71ac71a0bb82e99c1a30be
Zdeněk Borovec <zdenda.borovec@gmail.com>
2023-04-17 03:15:03 +0200

    added the basis for expression containers

---
+++ src/inc/addContainer.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <gtkmm.h>
+
+//!	A container for the 'add' mathematical expression.
+/*!
+ *	Represents the binary addition (+) operation.
+ */
+class AddContainer : public Gtk::Widget, public IEvaluable
+{
+public:
+	//! Constructor.
+	AddContainer();
+	virtual ~MyContainer();
+
+	double Evaluate();
+
+protected:
+	//Overrides:
+	Gtk::SizeRequestMode get_request_mode_vfunc() const override;
+	void measure_vfunc(Gtk::Orientation orientation, int for_size, int& minimum, int& natural,
+	int& minimum_baseline, int& natural_baseline) const override;
+	void size_allocate_vfunc(int width, int height, int baseline) override;
+#if 0
+	void forall_vfunc(const ForeachSlot& slot) override;
+
+	void on_add(Gtk::Widget* child) override;
+	void on_remove(Gtk::Widget* child) override;
+	GType child_type_vfunc() const override;
+#endif
+}
---
+++ src/inc/iEvaluable.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+//! Interface for evaluable elements
+/*!
+ *	This interface is to be implemented by expression container widgets
+ *	as well as the number widgets themselves.
+ *	An element implementing this interface has to be able to evaluate its
+ *	numerical value.
+ */
+class IEvaluable
+{
+public:
+	//! Evaluate my numerical value
+	/*!
+	 *	Either return my value if I hold a number, or evaluete the expression
+	 *	I am representing if I am an expression container.
+	 *
+	 *	\returns Numerical value of my insides.
+	 */
+	virtual double Evaluate() = 0;
+
+	//! A destructor
+	virtual inline ~IEvaluable() = 0;
+}
+IEvaluable::~IEvaluable(){}
